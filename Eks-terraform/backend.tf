92b480d6cf7afa09e6e98533638b81fba216cc11
Vaishnav Balodhi <89251215+vaishnavbalodhi@users.noreply.github.com>
2023-11-23 12:38:39 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vishal-terraform" # Replace with your actual S3 bucket name
+    bucket = "vaishnav-terraform" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
